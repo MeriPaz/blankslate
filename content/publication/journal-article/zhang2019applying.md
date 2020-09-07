7f6fe98de97f51aa860bcee2071cb98c0396a9e7
DE LA PAZ VILAS Maria <Maria.DeLaPazVilas@dnrme.qld.gov.au>
2020-09-07 22:36:39 +1000

    Update Refs

--- content/publication/journal-article/zhang2019applying.md
+++ content/publication/journal-article/zhang2019applying.md
@@ -2,7 +2,7 @@
 title = "Applying multi-layer artificial neural network and mutual information to the prediction of trends in dissolved oxygen"
 date = "2019-01-01"
 publication_types = ["2"]
-authors = ["Yifan Zhang", "Peter Fitch", "Peter Thorburn", "Maria de la Paz Vilas"]
+authors = ["Yifan Zhang", "Peter Fitch", "Peter Thorburn", "Maria P Vilas"]
 publication = "In: Frontiers in Environmental Science, (7), _pp. 46_"
 abstract = ""
 abstract_short = ""
--- setup.R
+++ setup.R
@@ -3,8 +3,8 @@
 blogdown::build_site()
 blogdown::serve_site()
 
-#install.packages("remotes")
+# install.packages("remotes")
 # see: https://github.com/petzi53/bib2academic/
-#remotes::install_github("petzi53/bib2academic")
+# remotes::install_github("petzi53/bib2academic")
 bib2academic::bib2acad("mybib_20200121.bib", copybib = TRUE, abstract = FALSE, overwrite = FALSE)
 # If everything went smoothly make a copy of your static/files/citations/ and content/publication folder. Then copy your .bib files into static/files/citations/ and your .md files into content/publication.
